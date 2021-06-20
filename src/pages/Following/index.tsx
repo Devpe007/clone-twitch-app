eb174f40947931a5eea9be175a0071638ecdbe40
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-06-20 10:22:05 -0300

    created redenrization of titles and created Heading component

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,17 +1,78 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { FlatList, View } from 'react-native';
 
 import { Wrapper, Container, Main } from './styles';
 
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+};
 
 const Following: React.FC = () => {
+  const { data, indices } =  React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>,
+      },
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C1', render: () => <View /> },
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C2', render: () => <View /> },
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C3', render: () => <View /> },
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C4', render: () => <View /> },
+      
+    ];
+
+    // Array que contém apenas os indices dos elementos que são titulos.
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices,
+    };
+  }, []);
+
   return (
     <Wrapper>
       <Container>
         <Header />
 
-        <Main />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={ ({ item }) => item.render() }
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indices}
+            // Refresh Effect
+            onRefresh={ () => {} }
+            refreshing={false}
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
